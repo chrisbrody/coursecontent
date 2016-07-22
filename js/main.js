d64451b4b06252754f49892198081504a8910188
chrisbrody <chrisbrody@codifyacademy.com>
2016-07-21 20:19:44 -0700

    updated more logic and changed some css

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ $(document).click(function(event) {
     var currentSelectEl = $(event.target)
 
     // make sure currentSelectParent exists and the current selected element is a H2 or H2
-    if(currentSelectEl[0].nodeName == 'H2' || currentSelectEl[0].nodeName == 'H3' && typeof currentSelectParent != 'undefined') {
+    if(currentSelectEl[0].nodeName == 'H3' && typeof currentSelectParent != 'undefined') {
+        // update previos parent to remove currentElement class
+        currentSelectParent.className = 'ng-scope'
+    } else if(currentSelectEl[0].nodeName == 'H2' && typeof currentSelectParent != 'undefined') {
         // update previos parent to remove currentElement class
         currentSelectParent.className = 'ng-scope'
     }
@@ -13,7 +16,12 @@ $(document).click(function(event) {
 
     // OPENS INDIVIDUAL DIVS
     // make sure the current selected element is an H2 or H3
-    if(currentSelectEl[0].nodeName == 'H2' || currentSelectEl[0].nodeName == 'H3') {
+    if(currentSelectEl[0].nodeName == 'H3') {
+        // update currentSelectParent to the parent div 
+        currentSelectParent = $(event.target).parent()[0]
+        // update currentSelectParent class name to include currentElement
+        currentSelectParent.className = 'ng-scope currentElement'
+    } else if(currentSelectEl[0].nodeName == 'H2') {
         // update currentSelectParent to the parent div 
         currentSelectParent = $(event.target).parent()[0]
         // update currentSelectParent class name to include currentElement
